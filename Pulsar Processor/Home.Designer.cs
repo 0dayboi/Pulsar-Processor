3e8d258e4ff3027cc720ee0fc965fce65b224ec4
Nart Gashi <35667229+0dayboi@users.noreply.github.com>
2020-12-22 00:45:36 +0100

    Start

--- Pulsar Processor/Home.Designer.cs
+++ Pulsar Processor/Home.Designer.cs
@@ -28,26 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.richTxtBox_Console = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtBox_Filepath = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnStartPulsarProcessing = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // richTxtBox_Console
             // 
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 260);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(769, 225);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.richTxtBox_Console.BackColor = System.Drawing.SystemColors.WindowText;
+            this.richTxtBox_Console.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTxtBox_Console.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.richTxtBox_Console.ForeColor = System.Drawing.Color.LimeGreen;
+            this.richTxtBox_Console.Location = new System.Drawing.Point(14, 130);
+            this.richTxtBox_Console.Name = "richTxtBox_Console";
+            this.richTxtBox_Console.ReadOnly = true;
+            this.richTxtBox_Console.Size = new System.Drawing.Size(769, 300);
+            this.richTxtBox_Console.TabIndex = 0;
+            this.richTxtBox_Console.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 244);
+            this.label1.Location = new System.Drawing.Point(12, 103);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 13);
             this.label1.TabIndex = 1;
@@ -56,41 +70,115 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(11, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(101, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Select Pulsar Data: ";
             // 
-            // textBox1
+            // txtBox_Filepath
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(509, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtBox_Filepath.Location = new System.Drawing.Point(11, 41);
+            this.txtBox_Filepath.Name = "txtBox_Filepath";
+            this.txtBox_Filepath.ReadOnly = true;
+            this.txtBox_Filepath.Size = new System.Drawing.Size(586, 20);
+            this.txtBox_Filepath.TabIndex = 3;
             // 
-            // button1
+            // btnSelectFile
             // 
-            this.button1.Location = new System.Drawing.Point(528, 25);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(253, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Location = new System.Drawing.Point(603, 40);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(177, 23);
+            this.btnSelectFile.TabIndex = 4;
+            this.btnSelectFile.Text = "Select file";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnStartPulsarProcessing
+            // 
+            this.btnStartPulsarProcessing.Location = new System.Drawing.Point(11, 67);
+            this.btnStartPulsarProcessing.Name = "btnStartPulsarProcessing";
+            this.btnStartPulsarProcessing.Size = new System.Drawing.Size(188, 30);
+            this.btnStartPulsarProcessing.TabIndex = 5;
+            this.btnStartPulsarProcessing.Text = "Start pulsar processing";
+            this.btnStartPulsarProcessing.UseVisualStyleBackColor = true;
+            this.btnStartPulsarProcessing.Click += new System.EventHandler(this.btnStartPulsarProcessing_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(793, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(610, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Data must be only provided by CSV";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(205, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(575, 56);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "System Information";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "label4";
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "Data File";
+            this.fileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.fileDialog_FileOk);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 497);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(793, 447);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnStartPulsarProcessing);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtBox_Filepath);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTxtBox_Console);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pulsar Processor";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,11 +186,18 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTxtBox_Console;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBox_Filepath;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnStartPulsarProcessing;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
     }
 }
 
--- Pulsar Processor/Home.cs
+++ Pulsar Processor/Home.cs
@@ -7,14 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Pulsar_Processor
 {
     public partial class Home : Form
     {
+        DateTime dt = new DateTime();
+        public static string FileLoaded = "";
+        public static string FileData = "";
+        public static string[] DataChunks;
+        public static int[] PossiblePeriods;
+
         public Home()
         {
             InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            Log("Pulsar processor initiated..");
+        }
+
+
+        #region MISC
+
+        public void Log(string ks)
+        {
+            dt = DateTime.Now;
+            string dateAndTime = dt.Day.ToString() + "/" + dt.Month.ToString() + "/" + dt.Year.ToString() + " " + dt.Hour.ToString() + ":" + dt.Minute.ToString() + ":" + dt.Second.ToString();
+            richTxtBox_Console.Text = richTxtBox_Console.Text + Environment.NewLine + "@" + dateAndTime + "|> " + ks; 
+        }
+
+        private void ReadFile()
+        {
+            if (FileLoaded.Length < 2)
+            {
+                MessageBox.Show("Please input possible pulsar data");
+                Log("Pulsar processing exited due to no data provided...");
+            }else
+            {
+               FileData = File.ReadAllText(FileLoaded);
+               Log("Temporary data has been stored in memory, loaded succesfully. Total size: " + FileData.Length.ToString());
+               Log("Data reading process finished.");
+            }
+
+        }
+
+        private void splitDataIntoArray()
+        {
+            DataChunks = FileData.Split(',');
+            Log("Array splitted into " + DataChunks.Length.ToString() + " chunks");
+        }
+        
+        private void locatePossiblePeriods()
+        {
+
+        }
+
+        private void FixPeriods()//This code will fix the period data by adding random values to fullfill the epoch folding.
+        {
+
+        }
+
+        private void TryPeriod()
+        {
+            
+        }
+
+        #endregion
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            fileDialog.ShowDialog();
+        }
+
+        private void fileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            FileLoaded = fileDialog.FileName;
+            txtBox_Filepath.Text = FileLoaded;
+        }
+
+        private void btnStartPulsarProcessing_Click(object sender, EventArgs e)
+        {
+            Log("Pulsar processing is starting...");
+            Log("Inititating file buffering..");
+            ReadFile();
+            Log("Chopping data into array");
+            splitDataIntoArray();
+            Log("Trying to find accurate period of pulsars");
+
         }
     }
 }
